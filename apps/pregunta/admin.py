026512dc963c78b694c395e21ad4188cad2490c4
Lourdes <lumil@hotmail.com>
2014-12-11 09:03:11 -0600

    proyecto

--- apps/pregunta/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(Categorias)
-admin.site.register(Pregunta)
-admin.site.register(Respuestas_Opcionales)
-admin.site.register(Juego_user)
-admin.site.register(Partida)
-admin.site.register(Perfil)
